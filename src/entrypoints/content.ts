fc0e2e4d236aecb3a97fcbd80a04d97d50878a5a
brkunver <burakhanunver@gmail.com>
2025-02-17 15:18:55 +0300

    change content website

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,6 +1,6 @@
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
+  matches: ["*://*.chatgpt.com/*"],
   main() {
-    console.log('Hello content.');
+    console.log("Hello content.")
   },
-});
+})
