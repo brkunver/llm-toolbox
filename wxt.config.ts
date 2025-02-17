3fcf900a837cbfb3fc203602cf7b7df772118bc7
brkunver <burakhanunver@gmail.com>
2025-02-17 15:18:42 +0300

    disable runner & setup tailwind plugin for vite

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt"
+import tailwindcss from "@tailwindcss/vite"
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: 'src',
-  extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-svelte'],
-});
+  srcDir: "src",
+  extensionApi: "chrome",
+  modules: ["@wxt-dev/module-svelte"],
+  runner: {
+    disabled: true,
+  },
+  vite: () => ({
+    plugins: [tailwindcss()] as any,
+  }),
+})
