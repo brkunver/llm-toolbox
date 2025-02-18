cacfc1b1bee2b3eaeb376d215e22b050d1007af2
brkunver <burakhanunver@gmail.com>
2025-02-18 15:18:26 +0300

    add helper functions add chatgpt support

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,11 +1,28 @@
-type TWebsite = "chatgpt" | "gemini" | "claude" | "deepseek" | "unknown"
+type TWebsite = "chatgpt" | "deepseek" | "gemini" | "claude" | "unknown"
 
 export function getWebsite(): TWebsite {
   const url = window.location.href
   if (url.includes("chatgpt")) return "chatgpt"
+  if (url.includes("deepseek")) return "deepseek"
   if (url.includes("gemini")) return "gemini"
   if (url.includes("claude")) return "claude"
-  if (url.includes("deepseek")) return "deepseek"
+
   return "unknown"
 }
 
+export function getPromptElement(website: TWebsite) {
+  if (website == "chatgpt") {
+    return document.querySelector("#prompt-textarea") as HTMLDivElement
+  }
+  return null
+}
+
+export function changePrompt(prompt: string) {
+  const website = getWebsite()
+  const promptElement = getPromptElement(website)
+  if (promptElement) {
+    if (website == "chatgpt") {
+      promptElement.innerText = prompt
+    }
+  }
+}
