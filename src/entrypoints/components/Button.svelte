c6df5aa299c4d9b9fa4923d93c441486f18923a9
brkunver <burakhanunver@gmail.com>
2025-02-20 17:27:17 +0300

    create button

---
+++ src/entrypoints/components/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { type Snippet } from "svelte"
+  import { twMerge } from "tailwind-merge"
+
+  interface Props {
+    className?: string
+    children: Snippet
+    onClick: () => void
+    disabled?: boolean
+  }
+
+  let { className = "", children, onClick, disabled = false }: Props = $props()
+</script>
+
+<button class={twMerge("py-2 px-4 rounded-2xl border", className)} onclick={onClick} {disabled}>
+  {@render children()}
+</button>
