9dd87d4ca6df3ecf7c3c14a15a33086d7bfa1767
brkunver <burakhanunver@gmail.com>
2025-02-20 14:35:41 +0300

    add "extension active status" logic

--- src/entrypoints/popup/App.svelte
+++ src/entrypoints/popup/App.svelte
@@ -1,7 +1,34 @@
 <script lang="ts">
   import "../global.css"
+  import { isExtensionActive } from "../../utils/storage"
+
+  let isActive = $state(true)
+
+  isExtensionActive.watch((active) => {
+    isActive = active
+  })
+
+  $effect(() => {
+    isExtensionActive
+      .getValue()
+      .then((value) => {
+        isActive = value
+      })
+      .catch((err) => {
+        console.error("Error while getting extension active storage information", err)
+      })
+  })
+
+  async function toggleExtension() {
+    const isActive = await isExtensionActive.getValue()
+    await isExtensionActive.setValue(!isActive)
+  }
 </script>
 
-<main>
+<main class="min-w-48 min-h-24 p-2 flex flex-col justify-center items-center">
   <h1>LLM Toolbox</h1>
+  <p>Extension is = {isActive}</p>
+  <button onclick={toggleExtension} class="border p-2 bg-black text-white rounded-2xl cursor-pointer"
+    >Toggle Extension</button
+  >
 </main>
