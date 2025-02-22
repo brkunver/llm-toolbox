13b6b7e6f496a5e49dc1a9835e0b547751134749
brkunver <burakhanunver@gmail.com>
2025-02-22 23:47:23 +0300

    make content ui a shadow root

--- src/entrypoints/content/index.tsx
+++ src/entrypoints/content/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
-
 import type { Tllm } from "@/utils/helpers.ts"
 
 const urls: Record<Tllm, string> = {
@@ -14,26 +13,60 @@ const urls: Record<Tllm, string> = {
 
 export default defineContentScript({
   matches: [...Object.values(urls)],
+  // 2. Set cssInjectionMode
+  cssInjectionMode: "ui",
 
-  main(ctx) {
-    const ui = createIntegratedUi(ctx, {
+  async main(ctx) {
+    // 3. Define your UI
+    const ui = await createShadowRootUi(ctx, {
+      name: "example-ui",
       position: "inline",
       anchor: "body",
       onMount: (container) => {
+        // Container is a body, and React warns when creating a root on the body, so create a wrapper div
+        const app = document.createElement("div")
+        container.append(app)
+
         // Create a root on the UI container and render a component
-        const root = ReactDOM.createRoot(container)
+        const root = ReactDOM.createRoot(app)
         root.render(<App />)
         return root
       },
       onRemove: (root) => {
         // Unmount the root when the UI is removed
-        if (root) {
-          root.unmount()
-        }
+        root?.unmount()
       },
     })
 
-    // Call mount to add the UI to the DOM
+    // 4. Mount the UI
     ui.mount()
   },
 })
+
+// integrated ui
+
+// export default defineContentScript({
+//   matches: [...Object.values(urls)],
+
+//   main(ctx) {
+//     const ui = createIntegratedUi(ctx, {
+//       position: "inline",
+//       anchor: "body",
+//       onMount: (container) => {
+//         // Create a root on the UI container and render a component
+//         const root = ReactDOM.createRoot(container)
+//         root.render(<App />)
+//         return root
+//       },
+//       onRemove: (root) => {
+//         // Unmount the root when the UI is removed
+//         if (root) {
+//           root.unmount()
+//         }
+//       },
+//     })
+
+//     // Call mount to add the UI to the DOM
+//     ui.mount()
+//   },
+// })
