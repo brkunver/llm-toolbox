b27fb7775dbe27a5ccb47478294fbae872fd957d
brkunver <burakhanunver@gmail.com>
2025-02-22 15:43:39 +0300

    import drawer

--- src/entrypoints/content/App.tsx
+++ src/entrypoints/content/App.tsx
@@ -1,13 +1,19 @@
+import "@/entrypoints/global.css"
+
+import logo from "./128.png"
+
 import { useState, useEffect } from "react"
 import { motion, AnimatePresence } from "motion/react"
-import "@/entrypoints/global.css"
-import logo from "./128.png"
+
 import { getWebsite, changePrompt } from "@/utils/helpers"
 import { isExtensionActive } from "@/utils/storage"
 
+import { Drawer } from "@/entrypoints/components/ui/drawer"
+
 function App() {
-  const [showUi, setShowUi] = useState<boolean>(true)
+  const [showUi, setShowUi] = useState<boolean>(false)
   const [showMenu, setShowMenu] = useState<boolean>(true)
+  const [showDrawer, setShowDrawer] = useState<boolean>(true)
 
   isExtensionActive.watch((active: boolean) => {
     setShowUi(active)
@@ -35,7 +41,7 @@ function App() {
     <>
       <button
         onClick={onMenuButtonClick}
-        className="fixed top-14 right-14 z-50 w-fit h-fit p-2 cursor-pointer rounded-2xl bg-gray-900"
+        className="fixed top-14 right-14 z-30 w-fit h-fit p-2 cursor-pointer rounded-2xl bg-gray-900"
       >
         <img src={logo} alt="toggle-menu" className="text-white w-10 h-10 mb-2 ml-2" />
       </button>
@@ -47,13 +53,20 @@ function App() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 1, type: "spring" }}
-            className="flex flex-col fixed top-32 right-14 z-50 p-4 rounded-2xl bg-gray-900 w-fit h-fit"
+            className="flex flex-col fixed top-32 right-14 z-30 p-4 rounded-2xl bg-gray-900 w-fit h-fit"
           >
             <h1>LLM Toolbox</h1>
             <p>Detected Website : {getWebsite()}</p>
             <button onClick={() => changePrompt("Hello, how are you?")}>Hello, how are you?</button>
+            <button onClick={() => setShowDrawer(true)}>Show Drawer</button>
           </motion.section>
         )}
+      </AnimatePresence>
+
+      <AnimatePresence>
+        <Drawer isOpen={showDrawer} onClose={() => setShowDrawer(false)} key={showDrawer.toString()}>
+          <h1>Drawer</h1>
+        </Drawer>
       </AnimatePresence>
     </>
   )
