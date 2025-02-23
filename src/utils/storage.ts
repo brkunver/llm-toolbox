c20ddd495348520730a86d584071ddd9e214060b
brkunver <burakhanunver@gmail.com>
2025-02-23 15:36:03 +0300

    add menu storage item

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -3,3 +3,7 @@ import { storage } from "wxt/storage"
 export const isExtensionActive = storage.defineItem<boolean>("local:isExtensionActive", {
   fallback: true,
 })
+
+export const isMenuActive = storage.defineItem<boolean>("local:isMenuActive", {
+  fallback: true,
+})
