2b8fa7e7be73e17ad6a0da72f2ba950e2fe62149
brkunver <burakhanunver@gmail.com>
2025-02-23 17:31:37 +0300

    add new storage managers

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,3 +1,4 @@
+import type { TBookmark, TPrompts } from "@/utils/types"
 import { storage } from "wxt/storage"
 
 export const isExtensionActive = storage.defineItem<boolean>("local:isExtensionActive", {
@@ -6,4 +7,12 @@ export const isExtensionActive = storage.defineItem<boolean>("local:isExtensionA
 
 export const isMenuActive = storage.defineItem<boolean>("local:isMenuActive", {
   fallback: true,
+})
+
+export const bookmarkStorage = storage.defineItem<TBookmark[]>("local:bookmarks", {
+  fallback: [],
+})
+
+export const promptStorage = storage.defineItem<TPrompts[]>("local:prompts", {
+  fallback: [],
 })
