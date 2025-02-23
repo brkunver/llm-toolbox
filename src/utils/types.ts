8f740d8c39f0d70956a6439da3bb246de455790d
brkunver <burakhanunver@gmail.com>
2025-02-23 17:31:08 +0300

    add bookmark and prompt types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,2 +1,14 @@
 export type Tllm = "chatgpt" | "deepseek" | "gemini" | "claude" | "perplexity" | "t3"
 export type TWebsite = Tllm | "unknown"
+
+export type TBookmark = {
+  id: string
+  name: string
+  url: string
+}
+
+export type TPrompts = {
+  id: string
+  name: string
+  prompt: string
+}
