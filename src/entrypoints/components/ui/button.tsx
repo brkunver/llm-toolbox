92ee241382d03d50b198063fb174725dcda10861
brkunver <burakhanunver@gmail.com>
2025-02-25 14:59:33 +0300

    create button component

---
+++ src/entrypoints/components/ui/button.tsx
@@ -0,0 +1,20 @@
+import { twMerge } from "tailwind-merge"
+
+interface ButtonProps {
+  onClick: () => void
+  children: React.ReactNode | string
+  className?: string
+}
+
+export default function Button(props: ButtonProps) {
+  return (
+    <button
+      {...props}
+      role="button"
+      onClick={props.onClick}
+      className={twMerge("p-2 rounded-md bg-black text-white cursor-pointer", props.className)}
+    >
+      {props.children}
+    </button>
+  )
+}
