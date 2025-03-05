b4e72121d1a73f309e1ae6865a2de6569cc2133f
brkunver <burakhanunver@gmail.com>
2025-03-05 18:56:01 +0300

    add exit animation

--- src/entrypoints/components/ui/modal.tsx
+++ src/entrypoints/components/ui/modal.tsx
@@ -1,5 +1,5 @@
 import { twMerge } from "tailwind-merge"
-import { motion, AnimatePresence } from "framer-motion"
+import { motion, AnimatePresence } from "motion/react"
 import { ReactNode } from "react"
 
 interface ModalProps {
@@ -10,28 +10,28 @@ interface ModalProps {
 }
 
 export default function Modal({ children, isOpen, onClose, className }: ModalProps) {
-  if (!isOpen) return null
-
   return (
     <AnimatePresence>
-      <div className="fixed inset-0 z-50 flex items-start justify-center pt-24">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 0.6 }}
-          exit={{ opacity: 0 }}
-          onClick={onClose}
-          className="fixed inset-0 bg-black"
-        />
+      {isOpen && (
+        <div className="fixed inset-0 z-50 flex items-start justify-center pt-24">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 0.6 }}
+            exit={{ opacity: 0 }}
+            onClick={onClose}
+            className="fixed inset-0 bg-black"
+          />
 
-        <motion.div
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
-          className={twMerge("relative z-50 rounded-lg bg-black text-white p-6 shadow-lg ", className)}
-        >
-          {children}
-        </motion.div>
-      </div>
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            className={twMerge("relative z-50 rounded-lg bg-black text-white p-6 shadow-lg ", className)}
+          >
+            {children}
+          </motion.div>
+        </div>
+      )}
     </AnimatePresence>
   )
 }
