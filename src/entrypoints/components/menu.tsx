424f2aa522421a0193574414b0caad8b317f1825
brkunver <burakhanunver@gmail.com>
2025-03-07 18:23:32 +0300

    move menu to its own file

---
+++ src/entrypoints/components/menu.tsx
@@ -0,0 +1,41 @@
+import { motion, AnimatePresence } from "motion/react"
+import { getWebsite, changePrompt } from "@/utils/helpers"
+import Button from "@/entrypoints/components/ui/button"
+import { memo } from "react"
+
+interface MenuProps {
+  showMenu: boolean
+  onShowPromptList: () => void
+  onShowNewPrompt: () => void
+  onShowPopup: () => void
+}
+
+function MenuComponent({ showMenu, onShowPromptList, onShowNewPrompt, onShowPopup }: MenuProps) {
+  if (import.meta.env.MODE == "development") {
+    console.log("Ext Dev : Menu Rendered")
+  }
+
+  return (
+    <AnimatePresence>
+      {showMenu && (
+        <motion.section
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5, type: "spring" }}
+          className="flex flex-col fixed top-32 right-14 z-30 p-4 rounded-2xl font-main bg-primary! border-border border-solid border-1 w-fit h-fit"
+        >
+          <h1 className="text-3xl! font-bold!">LLM Toolbox</h1>
+          <p>Detected Website : {getWebsite()}</p>
+          <Button onClick={() => changePrompt("Hello, how are you?")}>Hello, how are you?</Button>
+          <Button onClick={onShowPromptList}>Show Drawer</Button>
+          <Button onClick={onShowNewPrompt}>Show Modal</Button>
+          <Button onClick={onShowPopup}>Show Popup</Button>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  )
+}
+
+const Menu = memo(MenuComponent)
+export default Menu
