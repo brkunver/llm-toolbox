d8acd4005c9f247520f05bb7bfbae4b24a83ec6e
brkunver <burakhanunver@gmail.com>
2025-03-08 13:34:13 +0300

    add comment

--- src/entrypoints/content/index.tsx
+++ src/entrypoints/content/index.tsx
@@ -26,6 +26,7 @@ export default defineContentScript({
       anchor: "body",
       append: "first",
       onMount: (container) => {
+        //Inject Inter font
         const fontUrl = browser.runtime.getURL("/fonts/Inter.ttf")
         const fontStyle = document.createElement("style")
         fontStyle.textContent = `
