3cbef4ec798899e89058556a41168e96686100da
brkunver <burakhanunver@gmail.com>
2025-03-09 17:18:26 +0300

    change 'code' category name to 'coding'

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,6 @@
 export type Tllm = "chatgpt" | "deepseek" | "gemini" | "claude" | "perplexity" | "t3"
 export type TWebsite = Tllm | "unknown"
-export type TPromptCategory = "writing" | "conversation" | "creative" | "code" | "marketing" | "other"
+export type TPromptCategory = "writing" | "conversation" | "creative" | "coding" | "marketing" | "other"
 
 export type TBookmark = {
   id: string
