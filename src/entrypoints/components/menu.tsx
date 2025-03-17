9fb142ff83389e65f8ca8addda08cfbcbd1a71aa
brkunver <burakhanunver@gmail.com>
2025-03-17 15:52:29 +0300

    add persist menu state, add button icons

--- src/entrypoints/components/menu.tsx
+++ src/entrypoints/components/menu.tsx
@@ -2,7 +2,8 @@ import { motion, AnimatePresence } from "motion/react"
 import Button from "@/entrypoints/components/ui/button"
 import { memo } from "react"
 import AddBookmarkButton from "./add-bookmark-button"
-import { usePopupStore, useUIStateStore } from "@/utils/stores"
+import { useUIStateStore } from "@/utils/stores"
+import { List, Bookmark, PlusCircle } from "lucide-react"
 
 function Menu() {
   if (import.meta.env.MODE == "development") {
@@ -10,11 +11,27 @@ function Menu() {
   }
 
   const showMenu = useUIStateStore(state => state.showMenu)
+  const setShowMenu = useUIStateStore(state => state.setShowMenu)
   const setShowPromptList = useUIStateStore(state => state.setShowPromptList)
   const setShowNewPromptModal = useUIStateStore(state => state.setShowNewPromptModal)
   const setShowBookmarks = useUIStateStore(state => state.setShowBookmarks)
   const setShowAddBookmarkModal = useUIStateStore(state => state.setShowAddBookmarkModal)
-  const showPopup = usePopupStore(state => state.show)
+
+  const currentWebsite = getWebsite()
+
+  useEffect(() => {
+    async function setMenuStatus(){
+      const value = await isMenuActive.getValue()
+      setShowMenu(value)
+    }
+
+    isMenuActive.watch((active: boolean) => {
+      setShowMenu(active)
+    })
+
+    setMenuStatus()
+   
+  }, [])
 
   return (
     <AnimatePresence>
@@ -24,17 +41,27 @@ function Menu() {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.5, type: "spring" }}
-          className="flex flex-col fixed top-32 right-14 z-30 p-4 rounded-2xl font-main font-medium bg-primary! border-border border-solid border-1 w-fit h-fit"
+          className="flex flex-col fixed top-32 right-14 z-30 p-4 gap-1 rounded-2xl font-main font-medium bg-primary! border-border border-solid border-1 w-fit h-fit"
         >
           <h1 className="text-3xl font-bold">LLM Toolbox</h1>
-          <p>Detected Website : {getWebsite()}</p>
-          <Button onClick={() => changePrompt("Hello, how are you?")}>Hello, how are you?</Button>
-          <AddBookmarkButton />
-          <AddBookmarkButton changeTitle={true} onShowAddBookmarkModal={() => setShowAddBookmarkModal(true)} />
-          <Button onClick={() => setShowPromptList(true)}>My Prompts</Button>
-          <Button onClick={() => setShowBookmarks(true)}>My Bookmarks</Button>
-          <Button onClick={() => setShowNewPromptModal(true)}>Add new prompt</Button>
-          <Button onClick={() => showPopup("Hello, how are you?")}>Show Popup</Button>
+          <div id="website-detected-div" className="flex flex-col justify-center items-center my-1">
+            <p>Detected Website </p>
+            <div className={`${currentWebsite == "unknown" ? "bg-red-800" : "bg-emerald-900 "} rounded-2xl px-4 py-1 text-white font-semibold`}>{currentWebsite}</div>
+          </div>
+          <AddBookmarkButton className="w-full text-center" />
+          <AddBookmarkButton className="w-full text-center" changeTitle={true} onShowAddBookmarkModal={() => setShowAddBookmarkModal(true)} />
+          <Button className="flex gap-1 w-full text-center" onClick={() => setShowPromptList(true)}>
+            <List />
+            <span>My Prompts</span>
+          </Button>
+          <Button className="flex gap-1 w-full text-center" onClick={() => setShowBookmarks(true)}>
+            <Bookmark />
+            <span>My Bookmarks</span>
+          </Button>
+          <Button className="flex gap-1 w-full text-center" onClick={() => setShowNewPromptModal(true)}>
+            <PlusCircle />
+            <span>Add new prompt</span>
+          </Button>
         </motion.section>
       )}
     </AnimatePresence>
