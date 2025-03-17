66bbbc7c1c6234a87c16e84fa7cc06d20f9a1f11
brkunver <burakhanunver@gmail.com>
2025-03-17 16:04:25 +0300

    add new styles

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import Button from "@/entrypoints/components/ui/button"
 import { isExtensionActive } from "@/utils/storage"
+import { CircleX, PlusCircle, Settings } from "lucide-react"
 
 const Popup: React.FC = () => {
   const [isActive, setIsActive] = useState<boolean>(true)
@@ -28,14 +29,29 @@ const Popup: React.FC = () => {
   }
 
   return (
-    <main className="min-w-48 min-h-24 p-2 flex flex-col justify-center items-center font-main font-medium bg-primary text-white">
-      <h1>LLM Toolbox</h1>
-      <p>Extension is = {String(isActive)}</p>
-      <Button className="min-w-20" onClick={toggleExtension}>
-        {isActive ? "Deactivate" : "Activate"}
+    <main className="min-w-48 min-h-24 p-2 flex flex-col justify-center items-center font-main font-medium gap-1 bg-primary text-white">
+      <h1 className="text-2xl font-bold text-center">LLM Toolbox</h1>
+      <div className="mb-2">
+        <span className={`px-2 py-1 rounded-md ${isActive ? 'bg-emerald-900' : 'bg-red-900'}`}>
+          Extension is {isActive ? 'active' : 'inactive'}
+        </span>
+      </div>
+      <Button className="min-w-36 flex items-center justify-center gap-1" onClick={toggleExtension}>
+        {isActive ? (
+          <>
+            <CircleX size={16} />
+            <span>Deactivate</span>
+          </>
+        ) : (
+          <>
+            <PlusCircle size={16} />
+            <span>Activate</span>
+          </>
+        )}
       </Button>
-      <Button className="bg-blue-900" onClick={() => browser.runtime.openOptionsPage()}>
-        Open Options
+      <Button className="bg-amber-900 hover:bg-amber-950 flex items-center gap-1 " onClick={() => browser.runtime.openOptionsPage()}>
+        <Settings />
+        <span>Options</span>
       </Button>
     </main>
   )
