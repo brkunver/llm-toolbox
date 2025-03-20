b957307dc6cca6bf768ad691ef422fb7e1981281
brkunver <burakhanunver@gmail.com>
2025-03-20 16:31:44 +0300

    refactor options page

--- src/entrypoints/options/App.tsx
+++ src/entrypoints/options/App.tsx
@@ -1,4 +1,5 @@
 import Button from "@/entrypoints/components/ui/button"
+import { CircleX, PlusCircle } from "lucide-react"
 
 const Popup: React.FC = () => {
   const [isActive, setIsActive] = useState<boolean>(true)
@@ -28,9 +29,23 @@ const Popup: React.FC = () => {
   return (
     <main className="min-h-screen p-2 flex flex-col font-main font-medium bg-primary text-white">
       <h1>LLM Toolbox</h1>
-      <p>Extension is = {String(isActive)}</p>
-      <Button className="min-w-20 w-24" onClick={toggleExtension}>
-        {isActive ? "Deactivate" : "Activate"}
+      <div className="mb-2">
+        <span className={`px-2 py-1 rounded-md ${isActive ? "bg-emerald-900" : "bg-red-900"}`}>
+          Extension is {isActive ? "active" : "inactive"}
+        </span>
+      </div>
+      <Button className="min-w-36 flex items-center justify-center gap-1" onClick={toggleExtension}>
+        {isActive ? (
+          <>
+            <CircleX size={16} />
+            <span>Deactivate</span>
+          </>
+        ) : (
+          <>
+            <PlusCircle size={16} />
+            <span>Activate</span>
+          </>
+        )}
       </Button>
     </main>
   )
