d6f39accff2062cfa089020d919f531524902704
brkunver <burakhanunver@gmail.com>
2025-03-20 16:20:18 +0300

    add example data when installed

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,10 +1,39 @@
+const exampleBookmark : TBookmark = {
+  id: "exampleid1",
+  website: "chatgpt",
+  title: "ChatGPT Home",
+  url: "https://chatgpt.com/",
+}
+
+const examplePrompt : TPrompt = {
+  id: "promptid1",
+  title: "Act as a Data Scientist",
+  content: `I want you to act as a data scientist. 
+  Imagine you're working on a challenging project for a cutting-edge tech company. 
+  You've been tasked with extracting valuable insights from a large dataset related to user behavior on a new app. 
+  Your goal is to provide actionable recommendations to improve user engagement and retention.`,
+  category: "coding",
+  createdAt: new Date()
+}
+
+const examplePrompt2 : TPrompt = {
+  id: "promptid2",
+  title: "Act as SEO specialist",
+  content : `I want you to act as an SEO specialist. 
+  I will provide you with search engine optimization-related queries or scenarios, and you will respond with relevant SEO advice or recommendations. 
+  Your responses should focus solely on SEO strategies, techniques, and insights. 
+  Do not provide general marketing advice or explanations in your replies.`,
+  category: "marketing",
+  createdAt: new Date()
+}
+
 export default defineBackground(() => {
   browser.runtime.onInstalled.addListener((details) => {
     if (details.reason == "install") {
       isExtensionActive.setValue(true)
       isMenuActive.setValue(true)
-      bookmarkStorage.setValue([])
-      promptStorage.setValue([])
+      bookmarkStorage.setValue([exampleBookmark])
+      promptStorage.setValue([examplePrompt, examplePrompt2])
     }
   })
 })
