a11fb102aedf6e7f5a5254aea932b8d7673de7c4
brkunver <burakhanunver@gmail.com>
2025-03-29 16:39:28 +0300

    remove old comment

--- src/entrypoints/content/index.tsx
+++ src/entrypoints/content/index.tsx
@@ -59,31 +59,3 @@ export default defineContentScript({
     ui.mount()
   },
 })
-
-//  Old Integrated UI Code
-
-// export default defineContentScript({
-//   matches: [...Object.values(urls)],
-
-//   main(ctx) {
-//     const ui = createIntegratedUi(ctx, {
-//       position: "inline",
-//       anchor: "body",
-//       onMount: (container) => {
-//         // Create a root on the UI container and render a component
-//         const root = ReactDOM.createRoot(container)
-//         root.render(<App />)
-//         return root
-//       },
-//       onRemove: (root) => {
-//         // Unmount the root when the UI is removed
-//         if (root) {
-//           root.unmount()
-//         }
-//       },
-//     })
-
-//     // Call mount to add the UI to the DOM
-//     ui.mount()
-//   },
-// })
