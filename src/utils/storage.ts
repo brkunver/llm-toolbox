514c23621c59c6b5f8a9ffe030ae9493a4062b30
brkunver <burakhanunver@gmail.com>
2025-03-29 16:37:13 +0300

    change import path

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,4 +1,4 @@
-import { storage } from "wxt/storage"
+import { storage } from "#imports"
 import { TExtIconSize, ExtPositionType, TBookmark, TPrompt } from "./types"
 
 export const isExtensionActive = storage.defineItem<boolean>("local:isExtensionActive", {
